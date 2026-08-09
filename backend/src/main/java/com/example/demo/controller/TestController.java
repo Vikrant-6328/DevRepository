70680bb4d9687a6d362ba8cb41a8b8343a4e4d0c
Bikrant6328 <bikranta.gochhayat460@gmail.com>
2026-08-09 13:34:15 +0530

    3rd Commit added controller layer

---
+++ backend/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+	public String testMethod() {
+		return "Testing Method Executes";
+	}
+
+}
